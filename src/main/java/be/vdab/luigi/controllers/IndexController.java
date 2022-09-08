f7057db2b7dc28203be0e54905788a7447b4cb6e
SvenBouwen <sven.bouwen@vdabcampus.be>
2022-09-08 12:23:02 +0200

    hfdstk26

--- src/main/java/be/vdab/luigi/controllers/IndexController.java
+++ src/main/java/be/vdab/luigi/controllers/IndexController.java
@@ -8,10 +8,12 @@ import org.springframework.web.servlet.ModelAndView;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.concurrent.atomic.AtomicInteger;
 
 // @RestController
 @Controller
 public class IndexController {
+    private final AtomicInteger aantalBezoeken = new AtomicInteger();
     @GetMapping("/")
     public ModelAndView index() {
         var morgenOfMiddag = LocalTime.now().getHour() < 12 ? "morgen" : "middag";
@@ -19,6 +21,7 @@ public class IndexController {
         modelAndView.addObject("zaakvoerder",
                 new Persoon("Luigi", "Peperone", 7, true, LocalDate.of(1966, 1, 31),
         new Adres("Grote markt", "3", 9700, "Oudenaarde")));
+        modelAndView.addObject("aantalBezoeken", aantalBezoeken.incrementAndGet());
         return modelAndView;
     }
 }
