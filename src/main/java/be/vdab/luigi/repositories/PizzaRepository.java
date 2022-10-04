d67fa08c7e510669aac6b6e227e48282d024aacc
SvenBouwen <sven.bouwen@vdabcampus.be>
2022-10-04 16:54:05 +0200

    hfdstk38

--- src/main/java/be/vdab/luigi/repositories/PizzaRepository.java
+++ src/main/java/be/vdab/luigi/repositories/PizzaRepository.java
@@ -26,7 +26,7 @@ public class PizzaRepository {
                 .usingGeneratedKeyColumns("id");
     }
 
-    public long FindAantal() {
+    public long findAantal() {
         var sql = """ 
                 select count(*)
                 from pizzas
