3cca15d9dc5632fdc861ffadf5aa1e6f9780edc6
SvenBouwen <sven.bouwen@vdabcampus.be>
2022-11-24 11:17:55 +0100

    hfdstk48

--- src/main/java/be/vdab/luigi/domain/Pizza.java
+++ src/main/java/be/vdab/luigi/domain/Pizza.java
@@ -1,5 +1,7 @@
 package be.vdab.luigi.domain;
 
+import org.springframework.format.annotation.NumberFormat;
+
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PositiveOrZero;
@@ -8,6 +10,7 @@ import java.math.BigDecimal;
 public class Pizza {
     private final long id;
     private final String naam;
+    @NumberFormat(pattern = "0.00")
     private final BigDecimal prijs;
     private final boolean pikant;
 
